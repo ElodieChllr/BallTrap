14d201027c7b4cc4fcbabab0ca52aea179c1a104
Freya <elodie.chllr@gmail.com>
2024-03-21 19:05:32 +0100

    zeryt

--- Assets/Scripts/Manager/PauseManager.cs
+++ Assets/Scripts/Manager/PauseManager.cs
@@ -12,7 +12,7 @@ public class PauseManager : MonoBehaviour
     public GameObject player;
 
     public Animator settingsAnimator;
-
+    public AudioSource chanson;
     void Start()
     {
         pnl_Pause.SetActive(false);
@@ -25,15 +25,14 @@ public class PauseManager : MonoBehaviour
         if (playerInputRef.actions["Pause"].WasReleasedThisFrame())
         {
             isPaused = !isPaused;
-        }
-
-        if (isPaused)
-        {
-            OpenPause();
-        }
-        else
-        {
-            ClosePause();
+            if (isPaused)
+            {
+                OpenPause();
+            }
+            else
+            {
+                ClosePause();
+            }
         }
     }
 
@@ -46,14 +45,17 @@ public class PauseManager : MonoBehaviour
     {
         Debug.Log("open");
         Time.timeScale = 0f;
+        chanson.Pause();
         pnl_Pause.SetActive(true);
+        settingsAnimator.SetTrigger("openSettings");
     }
 
     public void ClosePause()
     {
+       // StartCoroutine(sdfs());
         pnl_Pause.SetActive(false);
         Debug.Log("close");
-        CloseSettings();
+        chanson.UnPause();
         Time.timeScale = 1f;
 
     }
@@ -61,19 +63,31 @@ public class PauseManager : MonoBehaviour
 
     public void OpenSettings()
     {
-        StartCoroutine(AnimSettings());
+        //StartCoroutine(AnimSettings());
     }
 
     public void CloseSettings()
     {
-        settingsAnimator.SetTrigger("closeSettings");
+        //settingsAnimator.SetTrigger("closeSettings");
     }
 
-    IEnumerator AnimSettings()
+    //IEnumerator AnimSettings()
+    //{
+    //    settingsAnimator.SetTrigger("openSettings");
+    //    //yield return new WaitForSeconds(5f);
+    //    //settingsAnimator.SetTrigger("idleSettings");
+
+    //}
+
+    IEnumerator sdfs()
     {
-        settingsAnimator.SetTrigger("openSettings");
         yield return new WaitForSeconds(1f);
-        settingsAnimator.SetTrigger("idleSettings");
+        settingsAnimator.SetTrigger("closeSettings");
+        yield return new WaitForSeconds(2f); // Attendez un peu plus longtemps
+        pnl_Pause.SetActive(false);
+        Debug.Log("close");
+        CloseSettings();
+        Time.timeScale = 1f;
 
     }
 }
