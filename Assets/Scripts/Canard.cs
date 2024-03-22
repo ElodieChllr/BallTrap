058788ab3b508a311d370a8212c0b3aa98be871d
Freya <elodie.chllr@gmail.com>
2024-03-22 14:08:17 +0100

    bon

--- Assets/Scripts/Canard.cs
+++ Assets/Scripts/Canard.cs
@@ -105,7 +105,7 @@ public class Canard : MonoBehaviour
             other.gameObject.GetComponents<Canard>();
             Debug.Log("on trigger");
 
-            if (playerInputRef.actions["Shoot"].WasPerformedThisFrame())
+            if (playerInputRef.actions["Shoot"].IsPressed())
             {
                 Debug.Log("shoot");
                 shakeCamAnim.SetTrigger("ShakeCam");
