2875fd6ef6fe19dbfc080feabf5883a20326600a
Freya <elodie.chllr@gmail.com>
2024-03-28 19:28:14 +0100

    Menu Select partout

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pnl_OldPanel;
+    public GameObject pnl_Selections;
+
+    public GameObject Bt_FirstSelection;
+    public GameObject Bt_OldSelect;
+
+    
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -18,5 +26,19 @@ public class GameManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenSelections()
+    {
+        pnl_OldPanel.SetActive(false);
+        pnl_Selections.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(Bt_FirstSelection/*.gameObject*/);
+    }
+
+    public void CloseSelections()
+    {
+        pnl_OldPanel.SetActive(true);
+        pnl_Selections.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(Bt_OldSelect/*.gameObject*/);
     }
 }
---
+++ Assets/Scripts/Manager/MainMenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuManager : MonoBehaviour
+{
+    
+
+    //public void OpenMainMenu()
+    //{
+
+    //}
+
+    //public void CloseMainMenu()
+    //{
+
+    //}
+}
